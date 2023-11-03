420a555965f93eabbaa4b74f9c121fcd81d6fbf0
Mateo <rubianes@fktech.net>
2023-11-03 12:43:01 -0300

    Se agrego CartWidget

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,17 @@
+import './CartWidget.css';
+import cart from './assets/cart.svg';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
+import { Link } from 'react-router-dom';
+
+const CartWidget = () => {
+  const { totalQuantity } = useContext(CartContext);
+  return (
+    <Link to='/cart' className='Cartwidget' style={{ display: totalQuantity > 0 ? 'block' : 'none' }}>
+      <img className='CartImg' src={cart} alt='cart-widget' />
+      {totalQuantity}
+    </Link>
+  );
+};
+
+export default CartWidget;
